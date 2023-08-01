d9d9e24460b2174d21932d155a11d46fb0e13d60
MikhailSavenko <misha47savenko@yandex.ru>
2023-08-01 09:31:34 +0000

    Project 11. Review 2

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,13 +1,18 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 
-DEBUG = False
+DEBUG = True if os.getenv('DEBUG', '').lower() == 'true' else False
 
-ALLOWED_HOSTS = ['158.160.72.90', '127.0.0.1', 'localhost']
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+AH = os.getenv('ALLOWED_HOSTS')
+ALLOWED_HOSTS = [host.strip() for host in AH.split(',')]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
